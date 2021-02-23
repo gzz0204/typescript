35e75bde65efa2904a9c67814bd1b77ace958332
gongzhizhen <gongzhizhen@corp.netease.com>
2021-02-23 22:52:39 +0800

    hello

---
+++ hello.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: gongzhizhen
+ * @Date: 2021-02-23 21:59:32
+ * @LastEditTime: 2021-02-23 22:19:36
+ * @LastEditors: gongzhizhen
+ */
+function sayHello(person) {
+    return 'Hello, ' + person;
+}
+var user = [0, 1, 2];
+var createdByNewBoolean = new Boolean(1);
+var num = undefined;
+console.log(sayHello(user));
---
+++ hello.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: gongzhizhen
+ * @Date: 2021-02-23 21:59:32
+ * @LastEditTime: 2021-02-23 22:19:36
+ * @LastEditors: gongzhizhen
+ */
+function sayHello(person: string) {
+    return 'Hello, ' + person;
+}
+
+let user = [0, 1, 2];
+let createdByNewBoolean: boolean = new Boolean(1);
+let num: number = undefined;
+console.log(sayHello(user));
